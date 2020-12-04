f96d88f97f0fb4ac35e7b5bd7e0ffdd9034acb9e
GrafSoul <grafsoul@gmail.com>
2020-12-04 12:32:40 +0500

    Feat:  Added the VariableEnvironments folder

--- ExecutionContextsAndLexicalEnvironments/Execution/app.js
+++ ExecutionContextsAndLexicalEnvironments/Execution/app.js
@@ -1,8 +1,15 @@
-b();
-console.log(a);
-
-var a = "Hello World!";
-
 function b() {
   console.log("Called b!");
 }
+
+b();
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+var a = "Hello World!";
+let b = "это Б";
+const c = "Это С";
+
+// console.log(a);
